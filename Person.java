6c05cf10a5aa9a68eadfe5dcd9443be8e90d945b
Kisang28 <85912291+Kisang28@users.noreply.github.com>
2022-04-06 16:07:50 +0800

    Change public into private

--- Person.java
+++ Person.java
@@ -1,4 +1,24 @@
 public class Person{
-    String firstName;
-    String lastname;
+    private String firstName;
+    private String lastname;
+
+
+// accesstor
+    public String getFirstname(){
+        return firstName;
+    }
+
+    public String getLastname (){
+        return lastname;
+    }
+
+//mutator
+
+public void setFirstName(String firstName){
+    this.firstName = firstName;
+}
+
+public void setLastName(String lastname){
+    this.lastname = lastname;
+}
 }
